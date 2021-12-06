96ae373fd64d68c617c343b53c65df86f1f4d696
jinrunheng <1175088275@qq.com>
2021-12-06 09:18:45 +0800

    2021-12-6 每日一题 PASS

---
+++ src/main/java/com/github/leetcodedaily/truncate_sentence/Solution.java
@@ -0,0 +1,14 @@
+package com.github.leetcodedaily.truncate_sentence;
+
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] strings = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            sb.append(strings[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
