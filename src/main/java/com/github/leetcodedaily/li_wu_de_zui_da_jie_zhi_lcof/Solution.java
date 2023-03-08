8a9d8a82ab53177840ebf361c81be0f22f4baa1d
jinrunheng <1175088275@qq.com>
2023-03-08 18:20:10 +0800

    20230308 每日一题

---
+++ src/main/java/com/github/leetcodedaily/li_wu_de_zui_da_jie_zhi_lcof/Solution.java
@@ -0,0 +1,30 @@
+package com.github.leetcodedaily.li_wu_de_zui_da_jie_zhi_lcof;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2023/3/8 6:13 下午
+ * @Version 1.0
+ */
+public class Solution {
+    public int maxValue(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < cols; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[rows - 1][cols - 1];
+    }
+}
